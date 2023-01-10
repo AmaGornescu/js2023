8a0f276859b451607ffad373f5cdfa6cd072e716
AmaGornescu <ana.gornescu@yahoo.com>
2023-01-10 15:26:21 +0200

    cursuri

---
+++ curs1/ap1.js
@@ -0,0 +1,7 @@
+
+alert('Salut JavaScript!')
+windows.alert('Salut')
+confirm('Vreau sa invat JavaScript')
+// console.log('mesaj in consola')
+// console.info('Info status')
+// console.error('Mesaj de eroare')
